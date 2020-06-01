565060fe96cf6bafbaf6a6685982f800febe6de5
vagrant <vagrant@localhost.localdomain>
2020-06-01 16:52:35 +0000

    Fix destroy機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -40,9 +40,9 @@ class BooksController < ApplicationController
   	end
   end
 
-  def delete
+  def destroy
   	@book = Book.find(params[:id])
-  	@book.destoy
+  	@book.destroy
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
