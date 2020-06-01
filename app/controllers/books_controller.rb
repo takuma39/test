42e5f4e27cc0684da242ce508bbf9f906ec7ecc9
vagrant <vagrant@localhost.localdomain>
2020-06-01 13:49:57 +0000

    Fix books_controller:showの@bookを@booksに変更　/ Add books_controller:@book追加　＆book_params追加 / Fix newform:@bookに変更 / Fix books/show:@booksに変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BooksController < ApplicationController
 
   def show
-  	@book = Book.find(params[:id])
+  	@books = Book.find(params[:id])
+    @book = Book.new
   end
 
   def index
@@ -43,7 +44,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body, :user_id)
   end
 
 end
