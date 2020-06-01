b0f9fca47931eb89f381702dc59cf31c1a90964d
vagrant <vagrant@localhost.localdomain>
2020-06-01 12:07:42 +0000

    Fix books.rb & user.rb 1:Nの関係訂正 / user.rb　バリデーション変更　/ application.controller login logout後の遷移先変更 / users/show @変数に訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,:validatable
 
-  belongs_to :books
+  has_many :books, dependent: :destroy
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
-  validates :name, length: {maximum: 20, minimum: 2}
+  validates :name, presence: true, length: { in: 2..20 }
 end
